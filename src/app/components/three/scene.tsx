27d709a6d26e462efaf185cda4fb9dab1386af8b
tinodevclumsy <seungjun.dev@gmail.com>
2024-10-16 13:36:58 -0700

    feat: install react three and create sphere model

---
+++ src/app/components/three/scene.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import Sphere from "./sphere";
+import Lights from "./lights";
+
+const Scene: React.FC = () => {
+  return (
+    <div className="h-full fixed w-full top-0" style={{ zIndex: -1 }}>
+      <Canvas>
+        <OrbitControls autoRotate autoRotateSpeed={4} />
+        <Lights />
+        <Environment
+          files="./images/bg.hdr"
+          background
+          backgroundBlurriness={0.8}
+        />
+        <Sphere />
+      </Canvas>
+    </div>
+  );
+};
+
+export default Scene;
