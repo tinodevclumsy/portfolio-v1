81f0eb35c59d2c612f2a2cd11413413f6722d146
tinodevclumsy <seungjun.dev@gmail.com>
2024-11-08 12:54:35 -0800

    fix: fix type error on blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,42 +6,43 @@ interface BlogPostProps {
   params: { slug: string };
 }
 
-interface Post {
-  title: Contentful.EntryFieldTypes.Text;
-  content: Contentful.EntryFieldTypes.RichText;
-  slug: Contentful.EntryFieldTypes.Text;
-  date: Contentful.EntryFieldTypes.Date;
-}
+type BlogPost = {
+  contentTypeId: "blogPost";
+  fields: {
+    title: Contentful.EntryFieldTypes.Text;
+    content: Contentful.EntryFieldTypes.RichText;
+    slug: Contentful.EntryFieldTypes.Text;
+    date: Contentful.EntryFieldTypes.Date;
+  };
+};
 
-const getPost = async (slug: string): Promise<Post> => {
-  const res = await client.getEntries({
+const getPost = (slug: string) => {
+  const res = client.getEntries<BlogPost>({
     content_type: "blogPost",
     "fields.slug": slug,
     limit: 1,
   });
 
-  const post = res.items[0]?.fields;
-
-  if (!post) {
+  if (!res) {
     // TODO: redirect to 404
     throw new Error(`Post with slug "${slug}" not found`);
   }
 
-  return post;
+  return res;
 };
 
 const BlogDetailPage = async ({ params }: BlogPostProps) => {
   const { slug } = params;
-  const post = await getPost(slug);
+  const post = (await getPost(slug)).items[0];
 
   return (
     <div className="items-center min-h-screen px-5 font-[family-name:var(--font-geist-sans)]">
       <main>
         <div className="max-w-5xl mx-auto pt-56 pb-16">
           <div className="mb-8 pb-5 border-b-2 border-blue-900">
-            <h1 className="text-3xl font-semibold">{post.title}</h1>
+            <h1 className="text-3xl font-semibold">{post.fields.title}</h1>
           </div>
-          <Content content={post.content} />
+          <Content content={post.fields.content} />
         </div>
       </main>
     </div>
