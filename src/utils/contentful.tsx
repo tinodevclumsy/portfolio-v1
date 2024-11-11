3ea0bfded4c8c8a952902a9565302e32902d76fd
tinodevclumsy <seungjun.dev@gmail.com>
2024-11-11 13:22:09 -0800

    fix: move getpost code to utils

--- src/utils/contentful.tsx
+++ src/utils/contentful.tsx
@@ -1,8 +1,27 @@
 import { createClient } from "contentful";
+import * as Contentful from "contentful";
 
-const space = process.env.CONTENTFUL_SPACE_ID as string
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN as string
+const space = process.env.CONTENTFUL_SPACE_ID as string;
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN as string;
 export const client = createClient({
   space,
   accessToken,
 });
+
+type BlogPost = {
+  contentTypeId: "blogPost";
+  fields: {
+    title: Contentful.EntryFieldTypes.Text;
+    content: Contentful.EntryFieldTypes.RichText;
+    slug: Contentful.EntryFieldTypes.Text;
+    date: Contentful.EntryFieldTypes.Date;
+  };
+};
+
+export const getPosts = () => {
+  const res = client.getEntries<BlogPost>({
+    content_type: "blogPost",
+  });
+
+  return res;
+};
