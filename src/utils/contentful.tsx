b87bda2c269f25eaa1d07cbe7943539e313c564e
tinodevclumsy <seungjun.dev@gmail.com>
2024-11-11 21:47:07 -0800

    feat: change sdk to rest api for blog post detail

--- src/utils/contentful.tsx
+++ src/utils/contentful.tsx
@@ -8,7 +8,6 @@ export async function fetchPosts() {
     headers: {
       Authorization: `Bearer ${CONTENTFUL_ACCESS_TOKEN}`,
     },
-    next: { revalidate: 60 }, // ISR 적용을 위한 설정 (선택 사항)
   });
 
   if (!response.ok) {
@@ -16,6 +15,28 @@ export async function fetchPosts() {
   }
 
   const data = await response.json();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return data
+  return data;
+}
+
+export async function fetchPostBySlug(slug: string) {
+  const url = `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${CONTENTFUL_ACCESS_TOKEN}&content_type=blogPost&fields.slug=${slug}`;
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch post: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  const post = data.items[0];
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  return post;
 }
